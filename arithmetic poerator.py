bb3eba00a13d96fb9f44e7d3147cbccf9f3afb3b
KrupaliHothi <krupalihothi2@gmail.com>
2025-03-14 18:59:55 +0530

    python program-2

---
+++ arithmetic poerator.py
@@ -0,0 +1,25 @@
+a=int(input("Enter the Number="))
+b=int(input("Enetr the second Number="))
+
+op = input("Enter operator=")
+
+if opr=='+':
+    print("Add=",a+b)
+
+elif opr=='-':
+    print("sub=",a-b)
+    
+elif opr=='*':
+    print("mul=",a*b)
+    
+elif opr=='/':
+    print("div=",a/b)
+
+if a==0 and b==0:
+        print("Result is undefined")
+elif a=0:
+        print("Not divible with zero")
+else:
+    print("div=",a/b)
+else:
+    print("Inalid")
