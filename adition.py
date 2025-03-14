505731eb198f466546c60b783c83031efb1cba19
KrupaliHothi <krupalihothi2@gmail.com>
2025-03-14 18:54:50 +0530

    python program of adition

---
+++ adition.py
@@ -0,0 +1,21 @@
+a = 30
+b = 10
+
+ # addition
+print ('Sum: ', a + b)  
+
+# subtraction
+print ('Subtraction: ', a - b)   
+
+# multiplication
+print ('Multiplication: ', a * b)  
+
+# division
+print ('Division: ', a / b) 
+
+# floor division
+print ('Floor Division: ', a // b)
+
+# modulo
+print ('Modulo: ', a % b)  
+
